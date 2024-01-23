120db82a2035fc39969d4f2ebe2d3c58783e343a
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-01-23 19:39:08 +0600

    Features Data Geted git add .!

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.EDUBUZZ_USERNAME}:${process.env.EDUBUZZ_USERPASSWORD}@cluster0.2xcsswz.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    // await client.connect();
+
+    const featuresCollections = client.db("EdubuzzDB").collection("features");
+
+    app.get("/features", async (req, res) => {
+      const cursor = await featuresCollections.find().toArray();
+      res.send(cursor);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(uri);
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("EduBuzz Server is Running Now !!!!!");
@@ -13,3 +52,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`EduBuzz Server is Running on :- ${port}`);
 });
+
+// edubuzz-studies
+// scpMyNFlrSRGFYgt
